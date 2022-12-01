b952ca436b99814fe7aec8d7526f95f96220d6d6
Torres-09 <dla612@naver.com>
2022-12-02 02:29:30 +0900

    Initial commit

---
+++ src/main/java/hwan2da/SMDFA/SmdfaApplication.java
@@ -0,0 +1,13 @@
+package hwan2da.SMDFA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmdfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hwan2da/SMDFA/SmdfaApplicationTests.java
@@ -0,0 +1,13 @@
+package hwan2da.SMDFA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmdfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
