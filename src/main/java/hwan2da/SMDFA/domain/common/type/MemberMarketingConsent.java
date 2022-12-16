108ceb2d143b565f789432f5c4ae33eca6cf738f
Torres-09 <dla612@naver.com>
2022-12-16 22:33:28 +0900

    feat : 마케팅 활용여부 동의 클래스 추가

---
+++ src/main/java/hwan2da/SMDFA/domain/common/type/MemberMarketingConsent.java
@@ -0,0 +1,26 @@
+package hwan2da.SMDFA.domain.common.type;
+
+import hwan2da.SMDFA.domain.common.EnumModel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberMarketingConsent implements EnumModel {
+
+    APPROVE("마케팅 활용 동의"),
+    DENY("마케팅 활용 거부"),
+    UNVERIFIED("알 수 없음(하위 버전의 유저)"),
+    ;
+
+    private final String description;
+
+    @NotNull
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+}
--- src/main/java/hwan2da/SMDFA/domain/member/model/Member.java
+++ src/main/java/hwan2da/SMDFA/domain/member/model/Member.java
@@ -1,12 +1,16 @@
 package hwan2da.SMDFA.domain.member.model;
 
 import hwan2da.SMDFA.domain.common.BaseEntity;
+import hwan2da.SMDFA.domain.common.type.MemberMarketingConsent;
 import hwan2da.SMDFA.domain.post.model.Post;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +25,17 @@ public class Member extends BaseEntity {
     @Column(length = 50)
     private String nickname;
 
+    @Column(length = 50, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberMarketingConsent marketingConsent;
+
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
-    private final List<Post> userPosts = new ArrayList<>();
+    private final List<Post> memberPosts = new ArrayList<>();
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Member(@Nullable String socialId, @Nullable MemberSocialType socialType, @Nullable String name, @NotNull MemberMarketingConsent marketingConsent) {
+        this.memberSocialInfo = MemberSocialInfo.of(socialId, socialType);
+        this.nickname = name;
+        this.marketingConsent = marketingConsent;
+    }
 }
