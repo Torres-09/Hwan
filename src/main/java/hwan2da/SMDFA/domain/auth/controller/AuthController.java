bb139199498eecbd510eba0a517f2c892ac62960
Torres-09 <dla612@naver.com>
2022-12-19 23:55:41 +0900

    feat : 회원가입 API 추가

--- src/main/java/hwan2da/SMDFA/domain/auth/controller/AuthController.java
+++ src/main/java/hwan2da/SMDFA/domain/auth/controller/AuthController.java
@@ -1,9 +1,35 @@
 package hwan2da.SMDFA.domain.auth.controller;
 
+import hwan2da.SMDFA.config.session.SessionConstant;
+import hwan2da.SMDFA.domain.auth.model.dto.SignUpRequest;
+import hwan2da.SMDFA.domain.auth.model.dto.SignUpResponse;
+import hwan2da.SMDFA.domain.auth.service.AuthService;
+import hwan2da.SMDFA.domain.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+import static hwan2da.SMDFA.config.session.SessionConstant.*;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
+    private final HttpSession httpSession;
+    private final AuthService authService;
+
+    @PostMapping("/v1/signup")
+    public ApiResponse<SignUpResponse> signUp(
+            @RequestBody SignUpRequest request
+            ) {
+        Long memberId = authService.signUp(request);
+        httpSession.setAttribute(MEMBER_ID, memberId);
+
+        SignUpResponse response = SignUpResponse.of(httpSession.getId(), memberId);
+        return ApiResponse.success(response);
+    }
 }
--- src/main/java/hwan2da/SMDFA/domain/auth/service/AuthService.java
+++ src/main/java/hwan2da/SMDFA/domain/auth/service/AuthService.java
@@ -1,8 +1,8 @@
 package hwan2da.SMDFA.domain.auth.service;
 
 
-
+import hwan2da.SMDFA.domain.auth.model.dto.SignUpRequest;
 
 public interface AuthService {
-
+    Long signUp(SignUpRequest request);
 }
