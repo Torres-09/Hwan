b2550008e295cb9b843393015d1f547beddb07f0
Torres-09 <dla612@naver.com>
2022-12-20 23:24:12 +0900

    feat : 소셜 로그인을 했을 때, 어떤 소셜을 이용하여 로그인 했는 지에 따라 Provider를 연결해주는 ProviderFinder 기능 추가
    현재는 네이버만 구현 중, 나중에 카카오 , 구글 추가 예정

---
+++ src/main/java/hwan2da/SMDFA/domain/auth/service/AuthProviderFinderImpl.java
@@ -0,0 +1,33 @@
+package hwan2da.SMDFA.domain.auth.service;
+
+import hwan2da.SMDFA.config.provider.AuthProvider;
+import hwan2da.SMDFA.config.provider.NaverAuthProvider;
+import hwan2da.SMDFA.domain.member.model.MemberSocialType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.EnumMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class AuthProviderFinderImpl implements AuthProviderFinder{
+    private static final Map<MemberSocialType, AuthProvider> authProviderMap = new EnumMap<>(MemberSocialType.class);
+
+    private final NaverAuthProvider naverAuthProvider;
+
+    @PostConstruct
+    void initializeAuthProviders() {
+        authProviderMap.put(MemberSocialType.NAVER, naverAuthProvider);
+    }
+
+    @Override
+    public AuthProvider findAuthProvider(MemberSocialType socialType) {
+        AuthProvider authProvider = authProviderMap.get(socialType);
+
+        // 지원하지 않는 소셜타입에 대한 예외처리 적용
+        
+        return authProvider;
+    }
+}
--- src/main/java/hwan2da/SMDFA/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/hwan2da/SMDFA/domain/auth/service/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package hwan2da.SMDFA.domain.auth.service;
 
+import hwan2da.SMDFA.config.provider.AuthProvider;
 import hwan2da.SMDFA.domain.auth.model.dto.SignUpRequest;
 import hwan2da.SMDFA.domain.member.repository.MemberRepository;
 import hwan2da.SMDFA.domain.member.service.MemberService;
@@ -11,10 +12,12 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService{
     private final MemberRepository memberRepository;
     private final MemberService memberService;
+    private final AuthProviderFinder authProviderFinder;
+
 
     public Long signUp(SignUpRequest request) {
-        // request로 소셜 정보를 받아와서 Email을 추출해야한다.
-
-        return memberService.registerMember(request.toCreateMemberRequest(null));
+        AuthProvider authProvider = authProviderFinder.findAuthProvider(request.getSocialType());
+        String socialEmail = authProvider.getSocialEmail(request.getToken());
+        return memberService.registerMember(request.toCreateMemberRequest(socialEmail));
     }
 }
