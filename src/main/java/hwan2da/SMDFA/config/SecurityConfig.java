7b24b6ea9fed9ecfcca9e2f6263e466b029f766d
Torres-09 <dla612@naver.com>
2022-12-21 23:23:29 +0900

    fix : SecurityConfig 수정
    session 사용, frameOption 등 보안 취약점에 대한 내용들 수정

--- src/main/java/hwan2da/SMDFA/config/SecurityConfig.java
+++ src/main/java/hwan2da/SMDFA/config/SecurityConfig.java
@@ -5,20 +5,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @EnableGlobalAuthentication
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         security
+                .authorizeRequests()
+                //.antMatchers()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic().and()
+                .formLogin().disable()
+                .logout().disable()
                 .csrf().disable()
-
-                .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers()
+                .frameOptions().sameOrigin()
+                .cacheControl().and()
+                .xssProtection().and()
+                .httpStrictTransportSecurity().disable();
 
         return security.build();
     }
