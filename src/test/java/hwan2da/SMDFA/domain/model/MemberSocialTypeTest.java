35ca7d4588f030d1435d3e86440688ffd6a7e98b
Torres-09 <dla612@naver.com>
2022-12-28 23:03:34 +0900

    test : socialType value test

---
+++ src/test/java/hwan2da/SMDFA/domain/model/MemberSocialTypeTest.java
@@ -0,0 +1,15 @@
+package hwan2da.SMDFA.domain.model;
+
+import hwan2da.SMDFA.domain.member.model.MemberSocialType;
+import org.junit.jupiter.api.Test;
+
+public class MemberSocialTypeTest {
+    @Test
+    void 이름_확인(){
+        System.out.println(MemberSocialType.KAKAO);
+        MemberSocialType[] values = MemberSocialType.values();
+        for (MemberSocialType type : values) {
+            System.out.println(type.getDescription());
+        }
+    }
+}
