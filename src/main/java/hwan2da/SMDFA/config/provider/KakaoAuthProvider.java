ba6b139d60e4dc80136cf96744be728b1345d97c
Torres-09 <dla612@naver.com>
2023-01-01 23:56:55 +0900

    feat : 기존 이메일 반환을 이메일 & ID 반환으로 추가 변경

--- src/main/java/hwan2da/SMDFA/config/provider/KakaoAuthProvider.java
+++ src/main/java/hwan2da/SMDFA/config/provider/KakaoAuthProvider.java
@@ -4,9 +4,12 @@ import hwan2da.SMDFA.config.client.kakao.KakaoAuthApiClient;
 import hwan2da.SMDFA.config.client.kakao.dto.KakaoProfileResponse;
 import hwan2da.SMDFA.config.client.naver.NaverAuthApiClient;
 import hwan2da.SMDFA.config.client.naver.dto.NaverProfileResponse;
+import hwan2da.SMDFA.domain.auth.model.dto.SocialResponse;
 import hwan2da.SMDFA.util.HttpHeaderUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
@@ -15,8 +18,9 @@ public class KakaoAuthProvider implements AuthProvider{
     private final KakaoAuthApiClient kakaoAuthApiClient;
 
     @Override
-    public String getSocialEmail(String token) {
+    public SocialResponse getSocialResponse(String token) {
         KakaoProfileResponse response = kakaoAuthApiClient.getProfileInfo(HttpHeaderUtils.withBearerToken(token));
-        return response.getKakao_account().getEmail();
+        SocialResponse socialResponse = SocialResponse.of(response.getId(), response.getKakao_account().getEmail());
+        return socialResponse;
     }
 }
