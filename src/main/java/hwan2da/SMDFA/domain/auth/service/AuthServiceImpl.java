ab4d879a65ccd1717c08220ad859c773ba403cdb
Torres-09 <dla612@naver.com>
2023-01-02 01:50:47 +0900

    feat : Login Service 로직 추가
    Controller ( API ) 계층 로직 추가 필요

--- src/main/java/hwan2da/SMDFA/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/hwan2da/SMDFA/domain/auth/service/AuthServiceImpl.java
@@ -1,7 +1,11 @@
 package hwan2da.SMDFA.domain.auth.service;
 
 import hwan2da.SMDFA.config.provider.AuthProvider;
+import hwan2da.SMDFA.domain.auth.model.dto.LoginRequest;
 import hwan2da.SMDFA.domain.auth.model.dto.SignUpRequest;
+import hwan2da.SMDFA.domain.auth.model.dto.SocialResponse;
+import hwan2da.SMDFA.domain.member.model.Member;
+import hwan2da.SMDFA.domain.member.model.MemberSocialType;
 import hwan2da.SMDFA.domain.member.repository.MemberRepository;
 import hwan2da.SMDFA.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +19,25 @@ public class AuthServiceImpl implements AuthService{
     private final AuthProviderFinder authProviderFinder;
 
 
+    @Override
     public Long signUp(SignUpRequest request) {
         AuthProvider authProvider = authProviderFinder.findAuthProvider(request.getSocialType());
-        String socialEmail = authProvider.getSocialEmail(request.getToken());
-        return memberService.registerMember(request.toCreateMemberRequest(socialEmail));
+        SocialResponse socialResponse = authProvider.getSocialResponse(request.getToken());
+        return memberService.registerMember(request.toCreateMemberRequest(socialResponse.getSocialId(), socialResponse.getSocialEmail()));
+    }
+
+    @Override
+    public Long login(LoginRequest request) {
+        AuthProvider authProvider = authProviderFinder.findAuthProvider(request.getSocialType());
+        SocialResponse socialResponse = authProvider.getSocialResponse(request.getToken());
+        return findMemberBySocialIdAndSocialType(socialResponse, request.getSocialType()).getId();
+    }
+
+    public Member findMemberBySocialIdAndSocialType(SocialResponse socialResponse, MemberSocialType type) {
+        Member member = memberRepository.findUserBySocial(socialResponse.getSocialId(), type);
+        if (member == null) {
+            throw new IllegalArgumentException();
+        }
+        return member;
     }
 }
