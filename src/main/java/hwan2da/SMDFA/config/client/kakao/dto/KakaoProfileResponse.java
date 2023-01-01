fdc1f5a255fcf7c82f8190f2bbb8ca141a17d593
Torres-09 <dla612@naver.com>
2023-01-02 00:27:33 +0900

    feat : 필드에 없는 속성이 오면 무시하는 어노테이션 추가
    JsonIgnoreProperties

--- src/main/java/hwan2da/SMDFA/config/client/kakao/dto/KakaoProfileResponse.java
+++ src/main/java/hwan2da/SMDFA/config/client/kakao/dto/KakaoProfileResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoProfileResponse {
     private String id;
     private String connected_at;
