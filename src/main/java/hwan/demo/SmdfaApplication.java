e321b8b6bd1f899cc3afa993ba0024bf19d5e430
Torres-09 <dla612@naver.com>
2023-03-20 01:28:34 +0900

    feat: batch 제거

--- src/main/java/hwan/demo/SmdfaApplication.java
+++ src/main/java/hwan/demo/SmdfaApplication.java
@@ -1,12 +1,10 @@
 package hwan.demo;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableBatchProcessing // 배치기능 활성화
 @SpringBootApplication
 public class SmdfaApplication {
 
