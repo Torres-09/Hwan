65ec05113f7446b155c803179517d2fa1ae8f90d
Torres-09 <dla612@naver.com>
2023-03-21 01:12:28 +0900

    feat: 배포 후 스크립트 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env bash
-
-REPOSITORY=/home/ec2-user/cicdproject
+REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-APP_NAME=cicdproject
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+APP_NAME=hwan
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z $CURRENT_PID ] #2
 then
-  echo "> 프로세스가 실행 중이지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 배포" #3
+nohup java -jar \
+        -Dspring.profiles.active=dev \
+        build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
